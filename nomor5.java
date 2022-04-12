f2b91bca8ec9946818350ca98e631fd2fe685844
dhimasesamahendra <88228256+dhimasesamahendra@users.noreply.github.com>
2022-04-12 11:39:27 +0700

    Update nomor5.java

--- nomor5.java
+++ nomor5.java
@@ -1,1 +1,16 @@
-
+public class lima {
+    public static int largest(int[] numbers){
+        int largest = numbers[0];
+        for(int i = 0; i < numbers.length; i++){
+            if (numbers[i] > largest){
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+    
+    public static void main(String[] args){
+        int[] numbers = {-1,100000,9,10};
+        System.out.println(largest(numbers));
+    }
+}
